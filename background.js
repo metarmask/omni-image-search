d41c8bb2c1c7ddbdb9b0ae43593665fb3ed1632a
metarmask <ihvarfner@gmail.com>
2015-11-17 12:16:06 +0100

    Remove analytics from background page

--- background.js
+++ background.js
@@ -1,16 +1,5 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-33927864-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 chrome.omnibox.onInputEntered.addListener(function(text) {
   chrome.tabs.update({
     url: 'https://www.google.com.hk/search?tbm=isch&q=' + text
   });
-  _gaq.push(['_trackEvent', 'Image', 'search', 'Google']);
 });
