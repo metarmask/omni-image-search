4d031e5113782907222e8e10e8ef49d3cda2ab51
metarmask <ihvarfner@gmail.com>
2015-11-17 12:20:08 +0100

    Switched top domain to .com

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.omnibox.onInputEntered.addListener(function(text) {
   chrome.tabs.update({
-    url: 'https://www.google.com.hk/search?tbm=isch&q=' + text
+    url: 'https://www.google.com/search?tbm=isch&q=' + text
   });
 });
